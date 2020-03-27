2692a9bb5093a0c4c6829a8ed95af6e34b808e96
Ekode <ekode@protonmail.com>
2020-03-27 09:40:30 +0100

    Added assets and show job page

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,14 +5,13 @@ namespace App\Controller;
 use App\Entity\Job;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JobController extends AbstractController
 {
     /**
      * @Route("/job", name="job")
-     * @param EntityManagerInterface $em
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index(EntityManagerInterface $em)
     {
@@ -20,6 +19,20 @@ class JobController extends AbstractController
         return $this->render('job/index.html.twig', [
             'controller_name' => 'JobController',
             'ListJobs' => $jobs
+        ]);
+    }
+
+    /**
+     * @Route("/job/{id}",name="job_show",requirements={"id"="\d+"})
+     */
+    public function show(EntityManagerInterface $em,int $id){
+        $job = $em->getRepository(Job::class)->find($id);
+        if(null === $job){
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('job/show.html.twig',[
+           'job' => $job,
         ]);
     }
 }
